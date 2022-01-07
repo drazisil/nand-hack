e757ad9b5b4ab220594dd8ee1dd5e537c7d2226a
Drazi Crendraven <drazisil@users.noreply.github.com>
2022-01-07 16:44:48 -0500

    create and chip and test

---
+++ src/and.js
@@ -0,0 +1,11 @@
+/**
+ * AND chip
+ * 
+ * @description return true if a and b == true, else return false
+ * @param {boolean} a
+ * @param {boolean} b
+ * @returns {boolean}
+ */
+export function and(a, b) {
+    return (a === true && b === true)
+}
