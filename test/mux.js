15b9e8bddf1498360b88f43c7d56412e8001bc88
Drazi Crendraven <drazisil@users.noreply.github.com>
2022-01-08 01:28:45 -0500

    rename and rewire mux to only use gates

---
+++ test/mux.js
@@ -0,0 +1,20 @@
+// @ts-check
+import assert from "assert"
+import { mux} from '../src/mux.js'
+
+describe('Multiplexer chip', function () {
+    it('should return a if sel == false', function () {
+        assert(mux(true, false, false))
+    })
+    it('should return b if sel == true', function () {
+        assert(mux(false, true, true))
+    })
+    describe('should return false', function () {
+        it('when (a,b) == false and sel == false', function () {
+            assert(!mux(false, false, false))
+        })
+        it('when (a,b) == false and sel == true', function () {
+            assert(!mux(false, false, true))
+        })
+    })
+})
