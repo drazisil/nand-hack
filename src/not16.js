05fbe43b5e0c49fb25328bf27368cd454a59fde8
Drazi Crendraven <drazisil@users.noreply.github.com>
2022-01-08 18:58:29 -0500

    correctly wire not16 and test

--- src/not16.js
+++ src/not16.js
@@ -1,49 +1,34 @@
 //@ts-check */
-import {or} from './or.js'
-import {not} from './not.js'
-import {and} from './and.js'
+import { not } from './not.js'
+
+/**
+ * @typedef {[boolean,boolean,boolean,boolean,boolean,boolean,boolean,boolean]} byte
+ * @typedef {[...byte, ...byte]} byte16
+ */
+
 /**
  * 16-bit NOT chip
  * 
- * @param {boolean} inputA0
- * @param {boolean} inputA1
- * @param {boolean} inputA2
- * @param {boolean} inputA3
- * @param {boolean} inputA4
- * @param {boolean} inputA5
- * @param {boolean} inputA6
- * @param {boolean} inputA7
- * @param {boolean} inputB0
- * @param {boolean} inputB1
- * @param {boolean} inputB2
- * @param {boolean} inputB3
- * @param {boolean} inputB4
- * @param {boolean} inputB5
- * @param {boolean} inputB6
- * @param {boolean} inputB7
- * @returns {boolean}
+ * @param {boolean} input00
+ * @param {boolean} input01
+ * @param {boolean} input02
+ * @param {boolean} input03
+ * @param {boolean} input04
+ * @param {boolean} input05
+ * @param {boolean} input06
+ * @param {boolean} input07
+ * @param {boolean} input08
+ * @param {boolean} input09
+ * @param {boolean} input10
+ * @param {boolean} input11
+ * @param {boolean} input12
+ * @param {boolean} input13
+ * @param {boolean} input14
+ * @param {boolean} input15
+ * @returns {byte16}
  */
-export function not16(inputA0,inputA1,inputA2,inputA3,inputA4,inputA5,inputA6,inputA7,inputB0,inputB1,inputB2,inputB3,inputB4,inputB5,inputB6,inputB7) {
-    const stateA01 = or(inputA0, inputA1)
-    const stateA23 = or(inputA2, inputA3)
-    const stateA45 = or(inputA4, inputA5)
-    const stateA67 = or(inputA6, inputA7)
+export function not16(input00, input01, input02, input03, input04, input05, input06, input07, input08, input09, input10, input11, input12, input13, input14, input15) {
 
-    const stateAstage1 = or(stateA01, stateA23)
-    const stateAstage2 = or(stateA45, stateA67)
-
-    const stateA = or(stateAstage1, stateAstage2) // if any of A are true
-
-    const stateB01 = or(inputB0, inputB1)
-    const stateB23 = or(inputB2, inputB3)
-    const stateB45 = or(inputB4, inputB5)
-    const stateB67 = or(inputB6, inputB7)
-
-    const stateBstage1 = or(stateB01, stateB23)
-    const stateBstage2 = or(stateB45, stateB67)
-
-    const stateB = or(stateBstage1, stateBstage2) // if any of B are true
-
-    return not(or(stateA, stateB))
+    return [not(input00), not(input01), not(input02), not(input03), not(input04), not(input05), not(input06), not(input07), not(input08), not(input09), not(input10), not(input11), not(input12), not(input13), not(input14), not(input15)]
 
 }
--- test/not16.test.js
+++ test/not16.test.js
@@ -8,40 +8,19 @@ import { not16 } from '../src/not16.js'
  */
 
 describe('16-bit NOT chip', function () {
-    describe('should return true', function () {
-        for (let i1 = 0; i1 <= 15; i1++) {
-            for (let i2 = 0; i2 <= 15; i2++) {
-                it(`when input1(${i1}) is false and input2(${i2}) is false`, function () {
-                    /** @type {byte} */
-                    const input1 = [false, false, false, false, false, false, false, false]
-                    /** @type {byte} */
-                    const input2 = [false, false, false, false, false, false, false, false]
-                    /** @type {byte16} */
-                    const input = [...input1, ...input2]
-                    assert(not16(...input))
-                })
-            }
+    it('should return out(0...15) == true', function () {
+        /** @type {byte} */
+        const input1 = [false, false, false, false, false, false, false, false]
 
-        }
-    })
-    describe('should return false', function () {
-        for (let i1 = 0; i1 <= 15; i1++) {
-            /** @type {byte} */
-            const input1 = [false, false, false, false, false, false, false, false]
-            input1[i1] = true
-            for (let i2 = 0; i2 <= 15; i2++) {
-                it(`when input1(${i1}) is true or input2(${12}) is true`, function () {
+        /** @type {byte} */
+        const output1 = [true, true, true, true, true, true, true, true]
 
-                    /** @type {byte} */
-                    const input2 = [false, false, false, false, false, false, false, false]
-                    input2[i2] = true
-                    /** @type {byte16} */
-                    const input = [...input1, ...input2]
+        /** @type {byte16} */
+        const input = [...input1, ...input1]
 
-                    assert(!not16(...input), `[${input1}],  [${input2}]`)
-                })
-            }
+        /** @type {byte16} */
+        const output = [...output1, ...output1]
 
-        }
+        assert.deepStrictEqual(not16(...input), [...output], `[${input}],  [${output}]`)
     })
 })
